f70c66010d18d8ddfbd2c5b7e694680ae67353c7
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-11 17:41:42 +0200

    Update and rename instant-raspbian.sh to runme.sh

--- runme.sh
+++ runme.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-apt install git
-git clone https://github.com/nachoparker/qemu-raspbian-network.git
-cd qemu-raspbian-network
+# Insert dialog stuff here.
 wget https://downloads.raspberrypi.org/raspbian_full/images/raspbian_full-2019-04-09/2019-04-08-raspbian-stretch-full.zip
 unzip 2019-04-08-raspbian-stretch-full.zip
 sudo ./qemu-pi.sh 2019-04-08-raspbian-stretch-full.img
